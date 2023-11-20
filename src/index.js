72afa8fc4c8cc8cd59f5ed0e327087bcc1543c43
박지원 <jackgg12322@gmail.com>
2023-11-20 15:53:38 +0900

    feat : HTML 기본 구조

--- src/index.js
+++ src/index.js
@@ -4,7 +4,12 @@ import ReactDOM from 'react-dom/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
- <h1>test</h1>
+<>
+<h1 id="title">가위바위보</h1>
+<button className="hand">가위</button>
+<button className="hand">바위</button>
+<button className="hand">보</button>
+</>
 );
 
 
